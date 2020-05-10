59ab0dff20e09474bad4f968fb979699c76815aa
LoicDV <Loic.DUPONT@student.umons.ac.be>
2020-05-10 11:17:57 +0200

    Mise en place des tests

--- randomizer.py
+++ randomizer.py
@@ -24,19 +24,20 @@ def generator_random(decimal_number):
 
 if __name__ == "__main__":
     with open ('test.txt', 'w') as f:
-        for ___ in range(1, 101):
+        for ___ in range(1, 2):
             print("test " + str(___) + ":" )
             n = 5
-
+            """
             list_test_us = []
             list_test_util_us = []
-            for __ in range(1000000):
+            for __ in range(1000):
                 number = randFloat(n)
                 list_test_us.append(number)
                 number *= (10**n)
                 list_test_util_us.append(int(number))
             dict_test_us = pi.occ_number(list_test_util_us)
             """
+
             list_test_random = []
             list_test_util_random = []
             for __ in range(1000):
@@ -45,9 +46,10 @@ if __name__ == "__main__":
                 number *= (10**n)
                 list_test_util_random.append(int(number))
             dict_test_random = pi.occ_number(list_test_util_random)
-            """
+
             """Test de Chi2"""
             """Notre générateur :"""
+            """
             dict_proba_chi2_us = pi.proba_dict_chi2(len(dict_test_us), dict_test_us)
             list_final_chi2_us = pi.test_chi2(len(list_test_us), list_test_us, dict_test_us, dict_proba_chi2_us, len(dict_test_us)-1)
             f.write("Test de Chi2" + '\n')
@@ -56,6 +58,7 @@ if __name__ == "__main__":
             f.write(str(kr) + '\n')
             string = "".join(str(elem) for elem in list_final_chi2_us)
             f.write(string + '\n')
+            """
             """-----------------------------------------------------"""
             """Random :"""
             """
@@ -68,3 +71,28 @@ if __name__ == "__main__":
             """
             """-----------------------------------------------------"""
             """-----------------------------------------------------"""
+            """ Test du Gap """
+            """ Notre générateur :"""
+            """
+            a = float(input("a dans [0, 1[ : "))
+            b = float(input("b dans ]0, 1] avec a < b : "))
+            list_final_gap_us = pi.test_gap(a, b, list_test_us)
+            print(list_final_gap_us)
+            """
+            """ Random :"""
+            """
+            list_final_gap_random = pi.test_gap(a, b, list_test_random)
+            print(list_final_gap_random)
+            """
+            """-----------------------------------------------------"""
+            """-----------------------------------------------------"""
+            """ Test du Poker """
+            """ Notre générateur :"""
+            """
+            list_final_poker_us = pi.test_poker(list_test_util_us)
+            print(list_final_poker_us)
+            """
+            """ Random :"""
+
+            list_final_poker_random = pi.test_poker(list_test_util_random)
+            print(list_final_poker_random)
